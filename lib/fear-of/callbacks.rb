c429e720a11a388b391975715dd7af968bf5ef74
Jamie van Dyke <jamie@fearoffish.com>
2009-10-08 14:07:35 +0100

    alias the method chain not overwrite

--- lib/fear-of/callbacks.rb
+++ lib/fear-of/callbacks.rb
@@ -1,9 +1,14 @@
 module FearOf
   module Callbacks
+    
+    def self.included(base)
+      base.send :alias_method_chain, :run_callbacks, :logging
+    end
+    
     def run_callbacks_with_logging(kind, options = {}, &block)
       ActiveRecord::Base.logger.debug "== Callback => #{self.class} - #{kind}"
+      run_callbacks_without_logging(kind, options = {}, &block)
     end
-    alias_method :run_callbacks, :run_callbacks_with_logging
   end
 end
 
